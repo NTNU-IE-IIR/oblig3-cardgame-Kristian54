95301cd0ec5098b15324f1473d3edbb02d119318
Kristian <garderkristian@gmail.com>
2024-03-03 12:35:27 +0100

    Added Main class and prepared pom.xml for utilization of JavaFX

---
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/Main.java
@@ -0,0 +1,8 @@
+package no.ntnu.idatx2003.oblig3.cardgame;
+
+public class Main {
+    public static void main(String[] args) {
+        PlayingCard card = new PlayingCard('H', 4);
+        System.out.println(card.getAsString());
+    }
+}
